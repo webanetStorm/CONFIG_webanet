d976897d53e807d6effa34dc5b19d8788191889f
webanet <my2712@mail.ru>
2024-09-23 22:27:03 +0300

    Ф. throw, .., / для ls

--- main.py
+++ main.py
@@ -9,25 +9,34 @@ class VirtualShell:
         self.tar_path = 'webanet.tar'
 
     def ls(self, dir):
+        current_dir = f'{self.current_path}/' if self.current_path else ''
         for item in dir:
-            if '/' not in item and item != self.current_path and item.startswith(self.current_path):
-                print(item)
+            if item.startswith(current_dir) and item != current_dir:
+                relative_item = item[len(current_dir):]
+                if '/' not in relative_item:
+                    print(relative_item)
 
     def cd(self, path, dir):
-        new_path = os.path.join(self.current_path, path)
-        if any(item == new_path for item in dir):
-            self.current_path = new_path
+        if path == '/':
+            self.current_path = ''
+        elif path == '..':
+            self.current_path = os.path.dirname(self.current_path)
         else:
-            print(f'\033[31mcd : Не удается найти путь "{self.current_path}/{new_path}", так как он не существует\033[0m')
+            new_path = os.path.join(self.current_path, path)
+            if any(item == new_path for item in dir):
+                self.current_path = new_path
+            else:
+                throw(f'cd : Не удается найти путь "{self.current_path}/{new_path}", так как он не существует')
 
+
+def throw(message):
+    print(f'\033[31m{message}\033[0m')
 
 def main():
     shell = VirtualShell()
-
     with tarfile.open('webanet.tar', 'a') as tar:
         while True:
             command = input(f'\033[36m{shell.current_path}> \033[0m').strip().split()
-
             if command[0] == 'exit':
                 break
             elif command[0] == 'ls':
@@ -35,8 +44,8 @@ def main():
             elif command[0] == 'cd' and len(command) >= 2:
                 shell.cd(command[1], tar.getnames())
             else:
-                print(f'\033[31m"{command[0]}" не является внутренней или внешней командой, исполняемой программой или пакетным файлом\033[0m')
+                throw(f'"{command[0]}" не является внутренней или внешней командой, исполняемой программой или пакетным файлом')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
