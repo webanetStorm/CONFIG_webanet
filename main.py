dd75aefa7df73f68033babcd66104d9dacd188a0
webanet <my2712@mail.ru>
2024-09-24 22:44:07 +0300

    Команды du, tree, find

--- main.py
+++ main.py
@@ -28,6 +28,24 @@ class VirtualShell:
             else:
                 throw(f'cd : Не удается найти путь "{self.current_path}/{new_path}", так как он не существует')
 
+    def du(self, dir, path='.'):
+        total_size = 0
+        path = f'{self.current_path}/{path}' if self.current_path else path
+        for item in dir:
+            if item.startswith(path):
+                with tarfile.open(self.tar_path, 'r') as tar:
+                    total_size += tar.getmember(item).size
+        print(f'{total_size} байт')
+
+    def tree(self, dir):
+        for item in dir:
+            print(' ' * 4 * item.count('/') + os.path.basename(item))
+
+    def find(self, dir, name):
+        found_items = [item for item in dir if name in item]
+        for item in found_items:
+            print(item)
+
 
 def throw(message):
     print(f'\033[31m{message}\033[0m')
@@ -43,6 +61,12 @@ def main():
                 shell.ls(tar.getnames())
             elif command[0] == 'cd' and len(command) >= 2:
                 shell.cd(command[1], tar.getnames())
+            elif command[0] == 'du':
+                shell.du(tar.getnames(), command[1] if len(command) >= 2 else '.')
+            elif command[0] == 'tree':
+                shell.tree(tar.getnames())
+            elif command[0] == 'find':
+                shell.find(tar.getnames(), command[1])
             else:
                 throw(f'"{command[0]}" не является внутренней или внешней командой, исполняемой программой или пакетным файлом')
 
